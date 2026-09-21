cfa12ff6a59dcd36a93acadb831ae8fa4aa7066e
Mychael Lucio Wolter dos Santos <157067201+MychaelWolter@users.noreply.github.com>
2026-09-21 11:28:55 -0400

    Create aula08.ts

---
+++ src/basico/aula08.ts
@@ -0,0 +1,82 @@
+console.log("OBJECT");
+
+const dados1 = {
+    nome: "Mychael",
+    idade: 21,
+    status: "Ativo",
+    ola: () => {
+        console.log("OLá");
+    },
+    info: (p: string) => {
+        console.log(`Testando ${p}`);
+    }
+};
+
+console.log(typeof(dados1));
+console.log(dados1);
+console.log(dados1.nome);
+
+dados1.nome = "Mychael Wolter";
+console.log(dados1.nome);
+
+dados1.ola();
+dados1.info("123");
+dados1.info(dados1.nome);
+
+console.log();
+
+const dados2: object = {
+    nome: "Mychael",
+    idade: 21,
+    status: "Ativo",
+    ola: () => {
+        console.log("OLá");
+    },
+    info: (p: string) => {
+        console.log(`Testando ${p}`);
+    }
+}
+
+console.log(typeof(dados2));
+console.log(dados2);
+console.log(dados2.nome);
+
+dados2.nome = "Mychael Wolter";
+console.log(dados2.nome);
+
+dados2.ola();
+dados2.info("123");
+dados2.info(dados2.nome);
+
+console.log();
+
+interface Dados {
+    nome: string;
+    idade: number;
+    status: string;
+    ola: void;
+    info: void;
+};
+
+const dados3 = {
+    nome: "Mychael",
+    idade: 21,
+    status: "Ativo",
+    ola: () => {
+        console.log("OLá");
+    },
+    info: (p: string) => {
+        console.log(`Testando ${p}`);
+    }
+};
+
+console.log(typeof(dados3));
+console.log(dados3);
+console.log(dados3.nome);
+
+dados3.nome = "Mychael Wolter";
+console.log(dados3.nome);
+
+dados3.ola();
+dados3.info("123");
+dados3.info(dados3.nome);
